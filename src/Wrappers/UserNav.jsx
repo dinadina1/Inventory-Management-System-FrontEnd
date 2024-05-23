d455ffa28e707427d86eb6db540bc0551726964b
dinadina1 <dinad9355@gmail.com>
2024-05-23 10:04:16 +0530

    Navbar updated

--- src/Wrappers/UserNav.jsx
+++ src/Wrappers/UserNav.jsx
@@ -12,6 +12,10 @@ const UserNav = ({ user, handleLogout }) => {
           <Link to={"/"} className="navbar-brand">
           </Link>
 
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
           <div className="collapse navbar-collapse" id="navbarNav">
 
             <ul className="navbar-nav ms-md-auto gap-2">
